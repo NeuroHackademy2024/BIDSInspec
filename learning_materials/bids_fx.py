d8b6ac4f4c05d9c13a4718d149ca5f992f19d01d
aptinis <aptinis@gmail.com>
2024-10-28 23:56:35 -0400

    Organize learning materials

--- learning_materials/bids_fx.py
+++ learning_materials/bids_fx.py
@@ -1,25 +1,21 @@
 # Project: BIDSInspec
-## Objective: Create a function that can query the BIDS directory
+# Objective: Create a function that can query the BIDS directory
 # Modified: August 8, 2024 @ 9:08 PM by N.O.
-
 # Implementation via pybids
-## Complete pybids tutorial
-
+# Complete pybids tutorial
 # Install pybids via conda
-## pip install pybids
-
+# pip install pybids
 # Loading BIDS datasets
-from bids import BIDSLayout
-from bids.tests import get_test_data_path
 import os
 
+from bids import BIDSLayout
+from bids.tests import get_test_data_path
+
 # Here we're using an example BIDS dataset that's bundled with the pybids tests
-data_path = os.path.join(get_test_data_path(), '7t_trt')
+data_path = os.path.join(get_test_data_path(), "7t_trt")
 
 # Initialize the layout
 layout = BIDSLayout(data_path)
 
 # Print some basic information about the layout
 layout
-
-
--- learning_materials/bids_pybids_tutorial.py
+++ learning_materials/bids_pybids_tutorial.py
@@ -1,20 +1,19 @@
 # Project: BIDSInspec
-## Objective: Complete the pybids tutorial
+# Objective: Complete the pybids tutorial
 # Modified: August 8, 2024 @ 9:08 PM by N.O.
-
 # Implementation via pybids
-## Must first complete pybids tutorial to understand the package
-
+# Must first complete pybids tutorial to understand the package
 # Install bids
 # pip install bids
 # Install pybids
-## pip install pybids
-
+# pip install pybids
 # Import
 from os.path import join
+
 from bids import BIDSLayout
 from bids.tests import get_test_data_path
-layout = BIDSLayout(join(get_test_data_path(), 'synthetic'))
+
+layout = BIDSLayout(join(get_test_data_path(), "synthetic"))
 
 # View data
 print(layout)
@@ -26,9 +25,10 @@ print(layout.get_sessions())
 print(layout.get_tasks())
 
 # Extract metadata
-# get task timing information for a given fMRI scan # cannot run since bids-dataset does not have NIfTI files
+# get task timing information for a given fMRI scan
+# cannot run since bids-dataset does not have NIfTI files
 # f = layout.get(task='nback', run=1, extension='nii.gz')[0].filename
-#print(layout.get_events(f))
+# print(layout.get_events(f))
 
 # get metadata from json files # cannot run since bids-dataset does not have NIfTI files
 # f = layout.get(task='nback', run=1, extension='nii.gz')[0].filename
